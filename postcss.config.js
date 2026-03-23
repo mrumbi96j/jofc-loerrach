9a84af1999916ee705ece274b8f2b4a08f8b7f90
mrumbi96j <mrumbi96@gmail.com>
2026-03-23 14:52:50 +0100

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="it">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Juventus Official Fan Club Lörrach G. Agnelli - biglietti, trasferte, partite e contatti." />
+    <title>Juventus Official Fan Club Lörrach G. Agnelli</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
